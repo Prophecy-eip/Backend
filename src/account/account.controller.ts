a6c79c0480b010694f303496998955812cf6438c
Victoire RABEAU <victoire.rabeau@epitech.eu>
2022-09-04 17:44:07 -0400

    feat(account/settings): Add update-username route + tests

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -23,10 +23,14 @@ export class AccountController{
         private readonly authService: AuthService,
     ) {}
 
+    private isFieldValid(str: string): boolean {
+        return (str !== undefined && str !== null && str !== "");
+    }
+
     @Post("sign-up")
     @HttpCode(HttpStatus.CREATED)
     async create(@Body("username") username: string, @Body("email") email: string, @Body("password") password: string) {
-        if (username === undefined || username === "" || email === undefined || email === "" || password === undefined || password === "") {
+        if (!this.isFieldValid(username) || !this.isFieldValid(email) || !this.isFieldValid(password)) {
             throw new HttpException("BAD_REQUEST", HttpStatus.BAD_REQUEST);
         }
         if (await this.profileService.credentialsAlreadyInUse(username, email)) {
@@ -69,12 +73,8 @@ export class AccountController{
     @HttpCode(HttpStatus.OK)
     async updatePassword(@Request() req, @Body("password") password: string) {
         const username = req.user.username;
-        const profile = await this.profileService.findOneByUsername(username);
 
-        if (profile === null) {
-            throw new UnauthorizedException();
-        }
-        if (password === null || password === undefined || password === "") {
+        if (!this.isFieldValid(password)) {
             throw new BadRequestException()
         }
         await this.profileService.updatePassword(username, password);
@@ -87,9 +87,22 @@ export class AccountController{
         const username = req.user.username;
         const profile = await this.profileService.findOneByUsername(username);
 
-        if (email === undefined || email === null || email === "") {
+        if (!this.isFieldValid(email)) {
             throw new BadRequestException()
         }
         await this.profileService.updateEmail(username, email);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Put("settings/update-username")
+    @HttpCode(HttpStatus.OK)
+    async updateUsername(@Request() req, @Body("username") newUsername: string) {
+        const username = req.user.username;
+        const profile = await this.profileService.findOneByUsername(username);
+
+        if (!this.isFieldValid(newUsername)) {
+            throw new BadRequestException()
+        }
+        await this.profileService.updateUsername(username, newUsername);
     }
 }
