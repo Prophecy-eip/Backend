b646217f9f365d19eb977b8e8eb8f807aad3ee73
Victoire RABEAU <victoire.rabeau@epitech.eu>
2022-10-11 21:30:29 -0400

    feat(army lists): Add army list and unit entities

--- src/account/profile/profile.entity.ts
+++ src/account/profile/profile.entity.ts
@@ -1,5 +1,6 @@
-import { BeforeInsert, Column, Entity, PrimaryColumn } from "typeorm";
+import { BeforeInsert, Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 import { hash } from "bcrypt";
+import { ArmyList } from "../../army-list/army-list.entity";
 
 export enum AccountType {
     PLAYER,
@@ -18,17 +19,22 @@ export class Profile {
     @Column()
     public password: string;
 
-    @Column({ default: false})
-    public is_verified: boolean;
+    @Column({ name: "is_verified", default: false})
+    public isVerified: boolean;
 
-    @Column({ default: "" })
-    public profile_picture_path: string;
+    @Column({ name: "profile_picture_path", default: "" })
+    public profilePicturePath: string;
 
-    @Column({ default: AccountType.PLAYER })
-    public account_type: AccountType;
+    @Column({ name: "account_type", default: AccountType.PLAYER })
+    public accountType: AccountType;
+
+    @Column({ name: "army_lists", nullable: true, type: "varchar" })
+    @ManyToOne(() => ArmyList, (list) => list.owner)
+    @JoinColumn({ name: "army_lists", referencedColumnName: "id" })
+    public armyLists: ArmyList[];
 
     @BeforeInsert()
-    async hashPassword() {
+    private async hashPassword() {
         this.password = await hash(this.password, 10);
     }
 }
---
+++ src/army-list/army-list.entity.ts
@@ -0,0 +1,43 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Army } from "../army/army.entity";
+import { JoinColumn } from "typeorm";
+import { Profile } from "../account/profile/profile.entity";
+import { ArmyListUnit } from "./army-list-unit/army-list-unit.entity";
+import { Upgrade } from "../army/upgrade/upgrade.entity";
+import { Rule } from "../army/rule/rule.entity";
+
+@Entity("army_lists")
+export class ArmyList {
+    @PrimaryGeneratedColumn("uuid")
+    public id: string;
+
+    @Column()
+    public name: string;
+
+    @Column({ type: "varchar" })
+    @OneToMany(() => Profile, (profile) => profile.armyLists)
+    @JoinColumn({ name: "profiles", referencedColumnName: "id" })
+    public owner: Profile;
+
+    @Column({ type: "varchar" })
+    @JoinColumn({ name: "armies", referencedColumnName: "id"})
+    public army: Army;
+
+    @Column()
+    public cost: string;
+
+    @Column({ type: "varchar" })
+    @JoinColumn({ name: "army_list_units", referencedColumnName: "id"})
+    public units: ArmyListUnit[];
+
+    @Column({ type: "varchar" })
+    @JoinColumn({ name: "upgrades", referencedColumnName: "id" })
+    public upgrades: Upgrade[];
+
+    @Column({ type: "varchar" })
+    @JoinColumn({ name: "rules", referencedColumnName: "id" })
+    public rules: Rule[];
+
+    @Column()
+    public isShared: boolean;
+}
