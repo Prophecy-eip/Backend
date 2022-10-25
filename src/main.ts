7d9f943c4145a9a48d2a74feddc30f3a8129e0ce
Victoire RABEAU <victoire.rabeau@epitech.eu>
2022-10-24 22:14:44 -0400

    fix(deployment): Fix images environment

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from "dotenv";
+
 import { AppModule } from './app.module';
+
+dotenv.config();
+
+const PORT = process.env.SERVER_PORT;
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
