c93ee15ffde54ab24b8f294c73d596041914e72c
Victoire Rabeau <victoire.rabeau@epitech.eu>
2023-03-29 17:27:17 -0400

    feat(ci): Add dev ci

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,17 @@ import { AppModule } from './app.module';
 dotenv.config();
 
 const PORT = process.env.SERVER_PORT;
+const DEV = process.env.DEV;
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (DEV !== undefined && DEV === "true") {
+    app.enableCors();
+    console.log("dev !")
+  }
+  
   await app.listen(PORT);
 }
 bootstrap();
