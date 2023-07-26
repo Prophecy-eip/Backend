2b5afc9bf09c91f7f082da9c0f9b2b67ee14dede
Victoire Rabeau <victoire.rabeau@epitech.eu>
2023-07-26 14:23:36 +0200

    feat(prophecies/armies): Refactor maths data

--- src/prophecy/army/prophecy-army.dto.ts
+++ src/prophecy/army/prophecy-army.dto.ts
@@ -1,5 +1,21 @@
+import { ProphecyArmy } from "@prophecy/army/prophecy-army.entity";
+
 class ProphecyArmyDTO {
 
+    constructor(prophecy: ProphecyArmy) {
+        this.id = prophecy.id;
+        this.armyList1 = prophecy.armyList1;
+        this.armyList2 = prophecy.armyList2;
+        this.player1Score = prophecy.player1Score;
+        this.player1Score = prophecy.player1Score;
+        this.player2Score = prophecy.player2Score;
+    }
+
+    public id: string;
+    public armyList1: string;
+    public armyList2: string;
+    public player1Score: number;
+    public player2Score: number;
 }
 
 export default ProphecyArmyDTO;
