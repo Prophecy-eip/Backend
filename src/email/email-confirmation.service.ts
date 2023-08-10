79367603ce9eeb3c5571723f717cfbd32cb10850
Victoire Rabeau <victoire.rabeau@epitech.eu>
2023-08-10 11:44:52 +0200

    doc(email): Add email services documentation

--- src/email/email-confirmation.service.ts
+++ src/email/email-confirmation.service.ts
@@ -6,16 +6,18 @@ import { EmailService } from "./email.service";
 import { ProfileService } from "@account/profile/profile.service";
 import { Profile } from "@account/profile/profile.entity";
 import { jwtConstants } from "@account/auth/constants";
+import { SendEmailCommandOutput } from "@aws-sdk/client-ses";
 
 dotenv.config();
 
-const API_URL: string = process.env.API_URL;
-const EMAIL_VERIFICATION_ROUTE: string = `${API_URL}/account/verify-email`;
 const FROM_ADDRESS: string = process.env.SES_FROM_ADDRESS;
 const WEBSITE_URL: string = process.env.WEBSITE_URL;
+const EMAIL_CONFIRMATION_ROUTE: string = `${WEBSITE_URL}/account/email-confirm`;
 
-const _EMAIL_CONFIRMATION_ROUTE: string = `${WEBSITE_URL}/account/email-confirm`;
-
+/**
+ * @class EmailConfirmationService
+ * @brief Service that enables users' email addresses confirmation
+ */
 @Injectable()
 export class EmailConfirmationService {
     constructor(
@@ -24,18 +26,27 @@ export class EmailConfirmationService {
         private readonly profileService: ProfileService,
     ) {}
 
-    public sendVerificationLink(email: string) {
+    /**
+     * @brief Sends an email confirmation link to the email address given as parameter
+     * @param email The email to send the confirmation link to
+     */
+    public sendVerificationLink(email: string): Promise<SendEmailCommandOutput> {
         const payload = { email: email };
         const token = this.jwtService.sign(payload, {
             secret: jwtConstants.secret,
             expiresIn: "3d"
         });
-        const url: string = `${EMAIL_VERIFICATION_ROUTE}?token=${token}`;
+        const url: string = `${EMAIL_CONFIRMATION_ROUTE}?token=${token}`;
         const text: string = `<p>Welcome to Prophecy!</p><p>To confirm your email address, click <a href="${url}">here</a>.</p>`;
 
         return this.emailService.sendEmail([email], FROM_ADDRESS, "Email confirmation", text);
     }
 
+    /**
+     * @brief Decodes the confirmation token given in the email confirmation link
+     * @param token The token to decode
+     * @return The email the token is assigned to
+     */
     public async decodeConfirmationToken(token: string): Promise<string> {
         try {
             const payload = await this.jwtService.verify(token, {
@@ -53,6 +64,11 @@ export class EmailConfirmationService {
         }
     }
 
+    /**
+     * @brief Confirms the users email addresses
+     *        Updates the database to set email verified as true.
+     * @param email The email address to confirm
+     */
     public async confirmEmail(email: string): Promise<void> {
         const profile: Profile = await this.profileService.findOneByEmail(email);
 
