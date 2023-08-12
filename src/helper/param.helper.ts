cd3c418dfb71a833f5d0b56b537ad7f117092982
Victoire Rabeau <victoire.rabeau@epitech.eu>
2023-08-12 08:22:01 +0200

    feat(helper): Add code documentation

--- src/helper/param.helper.ts
+++ src/helper/param.helper.ts
@@ -1,4 +1,13 @@
+/**
+ * @class ParamHelper
+ * @brief Defines common functions for parameters management
+ */
+
 export class ParamHelper {
+    /**
+     * @brief Checks if a parameter is nether null or undefined
+     * @param param The parameter to check
+     */
     static isValid(param: any): boolean {
         return !(param === null || param === undefined);
     }
