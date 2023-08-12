73f5ae288bb2230ee951880dde95ee481a11939b
Victoire Rabeau <victoire.rabeau@epitech.eu>
2023-08-12 08:20:01 +0200

    feat(heartbeat): Add code documentation

--- src/heartbeat/heartbeat.controller.ts
+++ src/heartbeat/heartbeat.controller.ts
@@ -1,8 +1,15 @@
 import { Controller, HttpCode, Get, HttpStatus } from "@nestjs/common";
 
+/**
+ * @class HeartbeatController
+ * @brief Controller that defines the heartbeat route
+ */
 @Controller("/heartbeat")
 export class HeartbeatController {
 
+    /**
+     * @brief Returns 200 (OK) if the server is up
+     */
     @HttpCode(HttpStatus.OK)
     @Get("")
     async heartbeat() {}
