4522d9bea72980851185b50c1654fd48897853e5
Victoire Rabeau <victoire.rabeau@epitech.eu>
2023-10-13 10:53:55 +0200

    feat(doc/api): Update swagger

--- src/helper/param.helper.ts
+++ src/helper/param.helper.ts
@@ -11,20 +11,4 @@ export class ParamHelper {
     static isValid(param: any): boolean {
         return !(param === null || param === undefined);
     }
-
-    /**
-     * @brief Checks if the object's required properties are not undefined or null
-     * @param object The object to check
-     * @param requiredProperties The properties that must not be null or undefined
-     * @param objectName The name of the object
-     * @return An empty array if everything is fine, an array of Error otherwise
-     */
-    static checkParamProperties(object: Object, requiredProperties: string[] , objectName: string): Error[] {
-        return requiredProperties.map((p: string): Error[] => {
-            if (!this.isValid(object[p])) {
-                return [new Error(`Invalid ${p} in ${objectName} object (null or undefined)`)];
-            }
-            return [];
-        }).flat();
-    }
 }
