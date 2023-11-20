cbfbdf2e72a3dd9c94aa687609a411a1a92d93fe
Victoire Rabeau <victoire.rabeau@epitech.eu>
2023-11-20 22:28:29 +0100

    fix(account/update-username): Fix bug on update username when owning armies lists

--- src/account/profile/profile.module.ts
+++ src/account/profile/profile.module.ts
@@ -10,14 +10,28 @@ import { EmailService } from "@email/email.service";
 import { EmailConfirmationService } from "@email/email-confirmation.service";
 import { ForgottenPasswordService } from "@email/forgotten-password.service";
 import { PasswordUpdateService } from "@email/password-update.service";
+import { ArmyListModule } from "@army-list/army-list.module";
+import { ProphecyModule } from "@prophecy/prophecy.module";
 
 /**
  * @class ProfileModule
  * @brief Module that groups all requirements for profile and user authentication
  */
 @Module({
-    imports: [TypeOrmModule.forFeature([Profile])],
-    providers: [ProfileService, AuthService, JwtService, EmailService, EmailConfirmationService, ForgottenPasswordService, PasswordUpdateService],
+    imports: [
+        TypeOrmModule.forFeature([Profile]),
+        ArmyListModule,
+        ProphecyModule
+    ],
+    providers: [
+        ProfileService,
+        AuthService,
+        JwtService,
+        EmailService,
+        EmailConfirmationService,
+        ForgottenPasswordService,
+        PasswordUpdateService
+    ],
     controllers: [AccountController],
     exports: [ProfileService, EmailConfirmationService, AuthService, JwtService, ForgottenPasswordService, PasswordUpdateService]
 })
